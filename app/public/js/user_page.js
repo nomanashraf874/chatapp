c4f12f082a1c919d63d159db0f138ff7be622ef4
mlavi16 <maya.lavi11@gmail.com>
2024-05-17 20:14:24 -0400

    refactoring UserPage class

--- app/public/js/user_page.js
+++ app/public/js/user_page.js
@@ -1,42 +1,42 @@
 // Modal functionality
-const modal = document.getElementById("myModal");
-const span = document.getElementsByClassName("close")[0];
-const currentUrl = window.location.href;
-const UserId = currentUrl.slice(31)
-console.log(UserId)
-function openModal() {
-    modal.style.display = "block";
-}
+class UserPage {
+    modal = document.getElementById("myModal");
+    userID = window.location.pathname.split("/")[2];
+    
+    constructor() {
+        this.initialize();
+    }
 
-span.onclick = function() {
-    modal.style.display = "none";
-}
+    initialize() {
+        console.log(this.userID);
+        const closeButton = document.getElementsByClassName("close")[0];
+        closeButton.onclick = function () {
+            this.modal.style.display = "none";
+        }.bind(this);
+        window.onclick = function (event) {
+            if (event.target == this.modal) {
+                this.modal.style.display = "none";
+            }
+        }.bind(this);
+        // Get the button that opens the add conversation modal and add a click event
+        const addConversationModal = document.getElementById("addConversationModal");
+        addConversationModal.onclick = function () {
+            this.openModal("Enter User ID of who you want to talk to", "addUser");
+        }.bind(this);
+        // Get the button that opens the delete conversation modal and add a click event
+        const deleteConversationModal = document.getElementById("deleteConversationModal");
+        deleteConversationModal.onclick = function () {
+            this.openModal("Enter Conversation ID to delete", "deleteUser");
+        }.bind(this);
+    }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    openModal(modalText, modalAction) {
+        this.modal.style.display = "block";
+        const label = document.getElementById('labelForConversation');
+        label.innerHTML = modalText;
+        const modalForm = document.getElementById('addConversation');
+        modalForm.action = `/${modalAction}/${this.userID}`;
     }
 }
 
-// Get the button that opens the modal
-const addConversationModal = document.getElementById("addConversationModal");
-const deleteConversationModal = document.getElementById("deleteConversationModal")
-
-// When the user clicks the button, open the modal
-addConversationModal.onclick = function() {
-    openModal();
-    const modalForm = document.getElementById('addConversation');
-    const label = document.getElementById('labelForConversation')
-    label.innerHTML = "Enter User ID of who you want to talk to"
-    modalForm.action = `/addUser/${UserId}`
-}
-
-deleteConversationModal.onclick = function() {
-    openModal()
-    const modalForm = document.getElementById('addConversation');
-    const label = document.getElementById('labelForConversation')
-    label.innerHTML = "Enter Conversation ID to delete"
-    modalForm.action = `/deleteUser/${UserId}`
-}
-
-
+const userPage = new UserPage();
